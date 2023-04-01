1d203fe092cd5ea635bc36d63b06c48e859b5a39
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-04-01 13:01:11 +0300

    feat: add menu

--- shelter/index.js
+++ shelter/index.js
@@ -1,3 +1,31 @@
+/************************************************/
+// Бургер меню //
+/************************************************/
+// получаем элементы .menu__icon и .menu__body
+const menuIcon = document.querySelector('.icon-menu');
+const menuBody = document.querySelector('.menu__body');
+// если .menu__icon есть
+if (menuIcon) {
+	document.body.addEventListener("click", function (e) {
+		// если кликнули по иконке бургер - переключаем классы
+		// открываем меню или закрываем его
+		if (e.target.closest('.icon-menu')) {
+			document.body.classList.toggle('_lock');
+			menuIcon.classList.toggle('_active');
+			menuBody.classList.toggle('_active');
+			// иначе кликнули вне иконки бургер - закрываем меню
+			// не важно, кникнули ли мы по пункту меню или вне его
+		} else {
+			document.body.classList.remove('_lock');
+			menuIcon.classList.remove('_active');
+			menuBody.classList.remove('_active');
+		};
+	});
+}
+
+
+
+
 console.log(`Страница Main (60)\n
 Проверка верстки +7 \n
 - верстка валидная +4 \n
